f499f7d326f2be7d35aff6e27aebdb9c04d88271
iliasdelferraro <delferraro.ilio@gmail.com>
2026-06-29 16:37:50 +0200

    git add

--- deep.py
+++ deep.py
@@ -1,4 +1,4 @@
-answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip()
 
 
 if answer == "42" or answer.lower() =="forty-two" or answer.lower() == "forty two":
